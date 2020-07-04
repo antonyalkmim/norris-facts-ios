4f2373053a4539cf44594e27760212bedb7769ae
Antony Alkmim <aalkmim@uolinc.com>
2020-07-04 13:54:22 -0300

    Initial Commit

---
+++ NorrisFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NorrisFacts
+//
+//  Created by Antony Nelson Daudt Alkmim on 04/07/20.
+//  Copyright © 2020 Antony Nelson Daudt Alkmim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
