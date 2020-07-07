c26e3ae5387a3e75e281de6e0ced3d3769c7040e
Antony Alkmim <aalkmim@uolinc.com>
2020-07-07 15:34:13 -0300

    feat: add load local facts

---
+++ NorrisFacts/Scenes/Facts/FactsList/ErrorView/FactListErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FactListErrorViewModel.swift
+//  NorrisFacts
+//
+//  Created by Antony Nelson Daudt Alkmim on 07/07/20.
+//  Copyright © 2020 Antony Nelson Daudt Alkmim. All rights reserved.
+//
+
+import Foundation
+
+struct FactListErrorViewModel {
+    let factListError: FactsListViewModel.FactListError
+}
