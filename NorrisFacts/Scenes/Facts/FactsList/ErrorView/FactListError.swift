d1fe510237206012d4fd04e3be3f214e92c32557
Antony Alkmim <antony-alkmim@hotmail.com>
2020-07-19 13:40:14 -0300

    refactor: add syntax sugar for FactListError
    - conform FactListError to Equatable protocol

--- NorrisFacts/Scenes/Facts/FactsList/ErrorView/FactListError.swift
+++ NorrisFacts/Scenes/Facts/FactsList/ErrorView/FactListError.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-enum FactListError: Error {
+enum FactListError: Error, NorrisFactsErrorType {
+    
     case syncCategories(Error)
     case loadFacts(Error)
     
@@ -19,5 +20,26 @@ enum FactListError: Error {
         case let .loadFacts(loadError):
             return (loadError as? NorrisFactsErrorType) ?? NorrisFactsError.unknow(loadError)
         }
+    }
+    
+    var code: Int {
+        switch self {
+        case .syncCategories: return 5
+        case .loadFacts: return 6
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .syncCategories: return "Error syncCategories"
+        case .loadFacts: return "Error load facts"
+        }
+    }
+    
+}
+
+extension FactListError: Equatable {
+    static func == (lhs: FactListError, rhs: FactListError) -> Bool {
+        lhs.code == rhs.code
     }
 }
